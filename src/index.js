38122563470a3fd2c7c02e8de2eb29dc0a7b6faa
KerimliVusal <kerimlivusal98@gmail.com>
2022-12-08 01:29:30 +0400

    message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { store } from './store'
 import { Provider } from 'react-redux'
 import ScrollToTop from './scroll';
@@ -11,10 +11,10 @@ import ScrollToTop from './scroll';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
-  <BrowserRouter>
+  <HashRouter>
   <ScrollToTop/>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </Provider>
 );
 
